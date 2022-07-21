6c36033f57e555003bae56b97345f0249665224d
Indrahsan <hasanindra71@gmail.com>
2022-07-21 21:06:43 +0700

    add connection database

--- utils/connection.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-import(
-	"github.com/beego/beego/v2/client/orm"
-)
-
-func ConnectionDB() {
-	orm.RegisterDataBase("default", "mysql", "root:@tcp(127.0.0.1:3306)/golang") 
-}
--- utils/helper.go
+++ utils/helper.go
@@ -2,9 +2,14 @@ package utils
 
 import(
 	"golang.org/x/crypto/bcrypt"
+	"github.com/beego/beego/v2/client/orm"
 )
 
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err
+}
+
+func ConnectionDB() {
+	orm.RegisterDataBase("default", "mysql", "root:@tcp(127.0.0.1:3306)/golang") 
 }
